0d36757543b8e64f451b51b347177d05d39a3a16
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-03-02 16:09:07 -0300

    first commit

---
+++ app/metodoFilter.js
@@ -0,0 +1,30 @@
+const botoes = document.querySelectorAll('.btn')
+
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
+    const valorTotal =  calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+    exibirOsLivrosNaTela(livrosFiltrados)
+
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal){
+    elementoComValorTotalDeLivrosDIsponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+  </div>`
+}
